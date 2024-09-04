f3a824b25e38b7094823092bfe13d780603f6484
drewschmidt145 <drewschmidt145@gmail.com>
2024-09-04 14:13:28 -0700

    update icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,11 +3,33 @@ import './Weather.css'
 import search_icon from '../assets/search.png'
 import humidity_icon from '../assets/humidity.png'
 import wind_icon from '../assets/wind.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
 
 const Weather = () => {
 
   const inputRef = useRef();
   const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  }
 
   const search = async (city) => {
     try {
@@ -16,12 +38,13 @@ const Weather = () => {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+      const icon = allIcons[data.weather[0].icon];
       setWeatherData({
         humidity: data.main.humidity,
         temperature: Math.floor(data.main.temp),
         windSpeed: data.wind.speed,
         location: data.name,
-        icon: data.weather[0].icon,
+        icon: icon,
       });
 
     } catch (error) {
@@ -50,7 +73,7 @@ const Weather = () => {
         <input ref={inputRef} type="text" placeholder='Search' onKeyPress={handleKeyPress}/>
         <img src={search_icon} alt="search" onClick={handleSearch} />
       </div>
-      <img src={`https://openweathermap.org/img/wn/${weatherData?.icon}@2x.png`} alt="weather icon" className='weather-icon'/>
+      <img src={weatherData?.icon} alt="weather icon" className='weather-icon'/>
       <p className='temperature'>{weatherData?.temperature}°F</p>
       <p className='location'>{weatherData?.location}</p>
       <div className="weather-data">
